32ac18ed8e0c26c9e17c5ae94229d7324846a445
Charles Shih <shichen120@gmail.com>
2017-10-25 10:12:28 +0800

    Add dmesg check in teardown.sh

--- test_suites/global_bin/teardown.sh
+++ test_suites/global_bin/teardown.sh
@@ -9,5 +9,27 @@ PATH=~/workspace/bin:/usr/sbin:/usr/local/bin:$PATH
 
 # Common clean up
 
+
+# log dmesg
+
+inst_type=$(metadata.sh -t | awk '{print $2}')
+inst_id=$(metadata.sh -i | awk '{print $2}')
+time_stamp=$(timestamp.sh)
+logfile=~/workspace/log/dmesg_${inst_type}_${inst_id}_${time_stamp}.log
+
+function log_dmesg(){
+	echo -e "\n$ dmesg $@\n------------------" >> $logfile
+	dmesg $@ >> $logfile
+}
+
+log_dmesg -l emerg
+log_dmesg -l alert
+log_dmesg -l crit
+log_dmesg -l err
+log_dmesg -l warn
+log_dmesg -l notice
+log_dmesg -l info
+log_dmesg -l debug
+
 exit 0
 
