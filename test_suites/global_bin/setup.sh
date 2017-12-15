099b2a41091f723e7a59455271fb748f75ce6196
Charles Shih <shichen120@gmail.com>
2017-12-15 14:23:40 +0800

    Update setup.sh

--- test_suites/global_bin/setup.sh
+++ test_suites/global_bin/setup.sh
@@ -12,8 +12,12 @@ PATH=~/workspace/bin:/usr/sbin:/usr/local/bin:$PATH
 if [ "$(os_type.sh)" = "redhat" ]; then
 
 	# disable unavailable repo
-#	echo "disable unavailable repo rhel7u4-debug"
-#	sudo yum-config-manager --disable rhel7u4-debug
+#	echo "disable unavailable repo rhel7u5-debug"
+#	sudo yum-config-manager --disable rhel7u5-debug
+
+#	cd ~/workspace/bin
+#	type lspci || sudo rpm -ivh pciutils-3.5.1-3.el7.x86_64.rpm
+#	type nvme || sudo rpm -ivh nvme-cli-1.3-1.el7.x86_64.rpm
 
 	# downgrade rdma-core from 13-7 to 13-5
 #	rpm -q rdma-core | grep rdma-core-13-7 >/dev/null 2>&1
