8094c490737db8db94191656064a77911459f759
Charles Shih <shichen120@gmail.com>
2018-01-08 18:29:04 +0800

    Fix a bug in cloud_type.sh

--- test_suites/global_bin/cloud_type.sh
+++ test_suites/global_bin/cloud_type.sh
@@ -4,6 +4,20 @@ PATH=~/workspace/bin:/usr/sbin:/usr/local/bin:$PATH
 
 # This script is used to tell the cloud type
 
+
+# for AWS instance excepts KVM based
+x=$(curl -fs http://169.254.169.254/latest/meta-data/public-hostname/hostname)
+if [ $? -eq 0 ]; then
+	[[ "$x" =~ "aws" ]] && echo "aws" && exit 0
+fi
+
+# for AWS instance KVM based (c5, m5, etc...)
+x=$(curl -fs http://169.254.169.254/latest/meta-data/public-hostname)
+if [ $? -eq 0 ]; then
+	[[ "$x" =~ "aws" ]] && echo "aws" && exit 0
+fi
+
+
 which virt-what 1> /dev/null 2> /dev/null
 if [ $? != 0 ];then
     yum -y install virt-what 1> /dev/null 2> /dev/null
