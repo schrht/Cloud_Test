525a9e6b95b96c59a9555f3e26acb09acb838cde
Charles Shih <shichen120@gmail.com>
2018-01-08 18:26:57 +0800

    Support dmesg check on RHEL6 in teardown.sh.

--- test_suites/global_bin/teardown.sh
+++ test_suites/global_bin/teardown.sh
@@ -17,19 +17,25 @@ inst_id=$(metadata.sh -i | awk '{print $2}')
 time_stamp=$(timestamp.sh)
 logfile=~/workspace/log/dmesg_${inst_type}_${inst_id}_${time_stamp}.log
 
-function log_dmesg(){
-	echo -e "\n$ dmesg $@\n------------------" >> $logfile
-	dmesg $@ >> $logfile
-}
 
-log_dmesg -l emerg
-log_dmesg -l alert
-log_dmesg -l crit
-log_dmesg -l err
-log_dmesg -l warn
-log_dmesg -l notice
-log_dmesg -l info
-log_dmesg -l debug
+if [[ "$(cat /etc/redhat-release)" = "Red Hat Enterprise Linux Server release 6."* ]]; then
+	dmesg >> $logfile
+else
+	function log_dmesg(){
+		echo -e "\n$ dmesg $@\n------------------" >> $logfile
+		dmesg $@ >> $logfile
+	}
+
+	log_dmesg -l emerg
+	log_dmesg -l alert
+	log_dmesg -l crit
+	log_dmesg -l err
+	log_dmesg -l warn
+	log_dmesg -l notice
+	log_dmesg -l info
+	log_dmesg -l debug
+fi
+
 
 # code coverage
 
