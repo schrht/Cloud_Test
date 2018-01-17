121e1ebc86cb00e372d70cf8b54eb18468bbe02d
Charles Shih <shichen120@gmail.com>
2018-01-17 09:30:19 +0800

    Add checkpoints for more cases in utils/check_report.sh

--- test_suites/utils/check_report.sh
+++ test_suites/utils/check_report.sh
@@ -25,3 +25,32 @@ if [[ "$PWD" =~ "RHEL7-87122" ]]; then
 	check 'grep -e "nvme list" -e "^Node" -e "^/dev/.*Amazon EC2 NVMe Instance Storage" result_*'
 	check 'grep -e "nvme read" -e "nvme write" -A 3 result_*'
 fi
+
+if [[ "$PWD" =~ "RHEL7-87124" ]]; then
+	check 'grep "rpm -qa | grep stress" -A 2 result_*'
+
+	echo -e "\nCHECKPOINTS:"
+	echo -e "When \"-c 2\"   : Avg_MHz:low,  Bzy_MHz>TSC_MHz, CPU%c1:<10, CPU%c6:high"
+	echo -e "When \"-c all\" : Avg_MHz:high, Bzy_MHz>TSC_MHz, CPU%c1:low, CPU%c6:low"
+	check 'grep -e "turbostat stress" -A 6 result_1_* | grep -v "stress: info:"'
+
+	echo -e "\nCHECKPOINTS:"
+	echo -e "First Item : Avg_MHz:low, Bzy_MHz>TSC_MHz, CPU%c1:~100%, CPU%c6:low"
+	echo -e "Second Item: Avg_MHz:low, Bzy_MHz=TSC_MHz, CPU%c1:~100%, CPU%c6:low"
+	check 'grep -e "turbostat stress" -A 6 result_2_* | grep -v "stress: info:"'
+fi
+
+if [[ "$PWD" =~ "RHEL7-87306" ]]; then
+	check 'grep "^\*.*nproc" resource_validation_*'
+	check 'grep "^\*\* " resource_validation_*'
+	check 'grep "virt-what" -A 10 resource_validation_*'
+fi
+
+if [[ "$PWD" =~ "RHEL7-87311" ]]; then
+	check 'grep "grep.*discard_max_bytes" -A 2 result_*'
+	check 'grep -e "^Allocating" -e "^Writing" -e "^Creating" -B 1 result_*'
+	check 'grep "fstrim /mnt/.*" -A 2 result_*'
+fi
+
+exit 0
+
