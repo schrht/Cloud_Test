4142041e1c561d0b393e2c002f552bb98bedefd3
Charles Shih <shichen120@gmail.com>
2018-06-26 19:02:14 +0800

    RHEL7-93100: Bugfix for Permission denied against sosreport.tar.xz.

--- test_suites/RHEL7-93100/bin/sos_report.sh
+++ test_suites/RHEL7-93100/bin/sos_report.sh
@@ -27,6 +27,7 @@ echo -e "\n\nTest Results:\n===============\n" >> $testlog
 # run test
 run_cmd 'sudo yum install sos -y'
 run_cmd 'sudo sosreport -a --batch'
+run_cmd 'sudo chmod 644 /var/tmp/sosreport*.tar.xz'
 run_cmd 'sudo cp $(ls -tr /var/tmp/sosreport*.tar.xz | tail -n 1) $HOME/workspace/log/'
 
 exit 0
